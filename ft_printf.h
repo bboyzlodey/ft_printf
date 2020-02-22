9d2c3508ca8be05e8e03fcf4b5d0c024ec18b869
asybil <skarlat-denis@mail.ru>
2020-02-22 16:18:35 +0300

    init project

---
+++ ft_printf.h
@@ -0,0 +1,13 @@
+//
+// Created by Agrajag Sybil on 22/02/2020.
+//
+
+#ifndef FT_PRINTF_FT_PRINTF_H
+# define FT_PRINTF_FT_PRINTF_H
+
+#include "./libft/libft.h"
+#include "./libft/get_next_line.h"
+
+int ft_printf(const char *restrict format, ...);
+
+#endif //FT_PRINTF_FT_PRINTF_H
