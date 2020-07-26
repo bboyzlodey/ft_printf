77fe6b0f382c32af5cbec439129a4d20c7509c11
asybil <skarlat-denis@mail.ru>
2020-07-26 23:42:15 +0300

    del comment

--- testing/ft_itoa.c
+++ testing/ft_itoa.c
@@ -2,9 +2,6 @@
 
 static char number_arr[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '\0'};
 
-/**
-*	нужно переписать в функции которые вернут строковое представление числа для int и unsigned int
-*/
 void	convert_int(long long int src, int delim)
 {
 	int			tmp = 0;
