de2a58f76c1c0e951614f588193a387e0365cae7
Agrajag Sybil <asybil@am-e2.msk.21-school.ru>
2020-08-15 07:30:55 -0700

    Refactor code

---
+++ t_long_num.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_long_num.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asybil <asybil@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/15 07:14:58 by asybil            #+#    #+#             */
+/*   Updated: 2020/08/15 07:25:45 by asybil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/*
+**  This file contain function which use for calculating
+**     rounding and other arifmethic operations by large numbers.
+**     Large number defined in ft_printf.h header as type t_long_num.
+**     See it.
+*/
+
+
+
+/*
+**  Sum of large numbers
+*/
+t_long_num summ_big_int(t_long_num one, t_long_num two)
+{
+	int max = 0;
+	max = one.digits > two.digits ? one.digits : two.digits;
+	int i = 0;
+	int c = 0;
+
+	while (i <= max)
+	{
+		c = c + one.value[i] + two.value[i];
+		one.value[i] = c % BIG_INT_BASE;
+		c = c / BIG_INT_BASE;
+		i++;
+	}
+	if (c > 0)
+	{
+		max = max + 1;
+		one.value[max] = c;
+	}
+	one.digits = max;
+	return one;
+}
+
+/*
+**  Function t_long_num mul_long(t_long_num a, int b)
+**  return result of operation 'a * b'
+*/
+t_long_num mul_long(t_long_num a, int b)
+{
+	int c, i;
+
+	c = 0;
+	i = 0;
+	while (i < a.digits)
+	{
+		a.value[i] = a.value[i] * b + c;
+		c = a.value[i] / 10;
+		a.value[i] = a.value[i] % 10;
+		i++;
+	}
+	while (c > 0)
+	{
+		a.digits++;
+		a.value[a.digits - 1] = c % 10;
+		c = c / 10;
+	}
+	return a;
+}
+
+/*
+** Возводит число 2 в степень
+*/
+t_long_num positive_pow(int exp)
+{
+	t_long_num tmp;
+	ft_bzero(&tmp.value, sizeof(tmp.value));
+	tmp.value[0] = 1;
+	tmp.digits = 1;
+	int i = 0;
+
+	while (exp > 0)
+	{
+		i = 0;
+		while (i < MAX_DIGITS)
+		{
+			tmp.value[i] *= 2;
+			i++;
+		}
+		i = 0;
+		while (i < MAX_DIGITS - 1)
+		{
+			if (tmp.value[i] >= BIG_INT_BASE)
+			{
+				tmp.value[i + 1] += tmp.value[i] / BIG_INT_BASE;
+				tmp.value[i] %= BIG_INT_BASE;
+			}
+			i++;
+		}
+		exp--;
+	}
+	tmp.digits = count_digits(tmp);
+	return tmp;
+}
+
+/*
+**  Возводит число base в степень exp.
+**  И возвращает тип большого числа
+*/
+t_long_num base_pow(int base,int exp) 
+{
+	t_long_num tmp;
+	ft_bzero(&tmp.value, sizeof(tmp.value));
+	tmp.value[0] = 1;
+	tmp.digits = 1;
+	int i = 0;
+
+	while (exp > 0)
+	{
+		i = 0;
+		while (i < MAX_DIGITS)
+		{
+			tmp.value[i] *= base;
+			i++;
+		}
+		i = 0;
+		while (i < MAX_DIGITS - 1)
+		{
+			if (tmp.value[i] >= BIG_INT_BASE)
+			{
+				tmp.value[i + 1] += tmp.value[i] / BIG_INT_BASE;
+				tmp.value[i] %= BIG_INT_BASE;
+			}
+			i++;
+		}
+		exp--;
+	}
+	tmp.digits = count_digits(tmp);
+	return tmp;
+}
